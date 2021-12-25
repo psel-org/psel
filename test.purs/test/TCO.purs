2d4152dd786d00d2ab60799809fe9c3db8b04c37
kamoii <>
2021-12-25 23:55:23 +0900

    Add TCO test

--- test.purs/test/TCO.purs
+++ test.purs/test/TCO.purs
@@ -7,12 +7,32 @@ foreign import succInt :: Int -> Int
 
 testTCO :: {} -> Array Boolean
 testTCO _ =
-  [ assertEqual "self recursion 100" (selfRec 0 100) "done"
-  , assertEqual "self recursion 1000" (selfRec 0 1000) "done"
-  , assertEqual "self recursion 10000" (selfRec 0 10000) "done"
+  [ assertEqual "self recursion 10" (selfRec1 0 10) "done"
+  -- , assertEqual "self recursion 100" (selfRec1 0 100) "done"
+  -- , assertEqual "self recursion 1000" (selfRec1 0 1000) "done"
+  , assertEqual "self recursion(2) 10" ((selfRec2 0 10) {}) "done"
+  , assertEqual "self recursion(3) 20" (selfRec3 20 0) 2
   ]
 
-selfRec :: Int -> Int -> String
-selfRec i to
+selfRec1 :: Int -> Int -> String
+selfRec1 i to
   | eqInt i to = "done"
-  | true = selfRec (succInt i) to
+  | true = selfRec1 (succInt i) to
+
+selfRec2 :: Int -> Int -> {} -> String
+selfRec2 i to
+  | eqInt i to = \_ -> "done"
+  | true = selfRec2 (succInt i) to
+
+-- 末尾呼出しと非末尾呼出しが混在している場合、結構ややこい
+-- selfRec3 to = g としてしまうと f の計算で無限ループになってしまう(多分JSも)。
+selfRec3 :: Int -> Int -> Int
+selfRec3 to i = g i
+  where
+    f = selfRec3 to
+    g i
+      | eqInt i to = 1
+      | eqInt i 10 = succInt (f (succInt i))   -- non-recursive call
+      | true = f (succInt i)                   -- recursive call
+
+-- 型クラス使って辞書受け取るバージョンも
