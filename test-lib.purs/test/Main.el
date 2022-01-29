0dcaf8264667d75067e2f2afdb6d491bca8a6f35
kamoii <>
2022-01-30 04:55:43 +0900

    Add test

---
+++ test-lib.purs/test/Main.el
@@ -0,0 +1,3 @@
+;; -*- lexical-binding: t; -*-
+
+(defvar Test.Main.nativeList '(1 2 3))
--- test-lib.purs/test/Main.purs
+++ test-lib.purs/test/Main.purs
@@ -5,11 +5,13 @@ import Effect
 import Effect.Ref as Ref
 import Effect.Console (log)
 import Test.Assert (assertEqual)
+import Data.List
 
 main :: Effect Unit
 main = do
   testApply
   testEffect
+  testList
 
 testApply :: Effect Unit
 testApply = do
@@ -52,3 +54,17 @@ testEffect1 = do
  where
   succ ref = Ref.modify (_ + 1) ref
   succVoid ref = void $ Ref.modify (_ + 1) ref
+
+foreign import nativeList :: List Int
+
+testList :: Effect Unit
+testList = do
+  let l = (1..3)
+  log "list(1)"
+  assertEqual { actual: nativeList, expected: l }
+  log "list(2)"
+  assertEqual { actual: f nativeList, expected: 6 }
+  assertEqual { actual: f l, expected: 6 }
+ where
+  f (Cons a (Cons b (Cons c Nil))) = a + b + c
+  f _ = 0
